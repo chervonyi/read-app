278544e2d974909fdde9b24486aa166ae39c99ca
chervonyi <yuri.ternopil@gmail.com>
2020-08-06 19:06:09 -0500

    Implement Save button
    Write functions to save a new title and update an existing one.

--- ReadApplication/app/src/main/java/room106/app/read/activities/EditTitleActivity.kt
+++ ReadApplication/app/src/main/java/room106/app/read/activities/EditTitleActivity.kt
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import room106.app.read.R
 import room106.app.read.models.Title
+import room106.app.read.models.User
+import java.util.*
 
 class EditTitleActivity : AppCompatActivity() {
 
@@ -28,7 +35,7 @@ class EditTitleActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
 
-    private var isNewTitle = false
+    private var titleID: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,13 +58,13 @@ class EditTitleActivity : AppCompatActivity() {
         bodyEditText.addTextChangedListener(titleDataWatcher)
 
 
-        if (intent.hasExtra("title_id")) {
+        titleID = intent.getStringExtra("title_id")
+
+        if (titleID != null) {
             // User editing an existing title
-            isNewTitle = false
             // TODO - Load title data and set it
         } else {
             // User's going to create a new title
-            isNewTitle = true
             saveTitleButton.text = getString(R.string.save_draft)
         }
 
@@ -68,6 +75,89 @@ class EditTitleActivity : AppCompatActivity() {
         checkFields()
     }
 
+    fun onClickSave(v: View) {
+        val currentUser = auth.currentUser ?: return
+
+        if (titleID == null) {
+            // Create a new document in "titles" collection
+            saveNewTitle(currentUser)
+        } else {
+            // Edit an existing document in "titles" collection
+            updateExistingTitle()
+        }
+    }
+
+    private fun saveNewTitle(currentUser: FirebaseUser) {
+        val title = titleEditText.text.toString()
+        val description = descriptionEditText.text.toString()
+        val titleBody = bodyEditText.text.toString()
+        val status = "draft"
+        val readsCount = 0
+        val likesCount = 0
+
+        db.collection("users").document(currentUser.uid).get()
+            .addOnSuccessListener { document ->
+                val user = document.toObject(User::class.java)
+
+                if (user != null) {
+                    val authorName = user.name
+                    val authorID = currentUser.uid
+                    val authorAvatar = user.avatar
+                    val time = Timestamp(Date())
+
+                    val titleObject = Title(title, description, authorName, authorID,
+                        authorAvatar, status, time, time, readsCount, likesCount)
+
+                    db.collection("titles").add(titleObject)
+                        .addOnSuccessListener { titleDocument ->
+                            val bodyObject = hashMapOf(
+                                "text" to titleBody
+                            )
+
+                            titleDocument.collection("body").document("text")
+                                .set(bodyObject).addOnSuccessListener {
+                                    titleID = titleDocument.id
+                                    saveTitleButton.text = getString(R.string.saved)
+                                }
+                        }
+
+                }
+            }
+    }
+
+    private fun updateExistingTitle() {
+        if (titleID != null) {
+            val newTitle = titleEditText.text.toString()
+            val newDescription = descriptionEditText.text.toString()
+            val newBody = bodyEditText.text.toString()
+
+            val titleRef = db.collection("titles").document(titleID!!)
+            val bodyRef = titleRef.collection("body").document("text")
+
+            var titleUpdated = false
+            var bodyUpdated = false
+
+            titleRef.update(mapOf(
+                "title" to newTitle,
+                "description" to newDescription
+            )).addOnSuccessListener {
+                titleUpdated = true
+
+                if (bodyUpdated) {
+                    saveTitleButton.text = getString(R.string.saved)
+                }
+            }
+
+            bodyRef.update("text", newBody).addOnSuccessListener {
+                bodyUpdated = true
+
+                if (titleUpdated) {
+                    saveTitleButton.text = getString(R.string.saved)
+                }
+            }
+        }
+    }
+
     //region Fields validation
     private val titleDataWatcher = object: TextWatcher {
         override fun afterTextChanged(p0: Editable?) { }
@@ -76,6 +166,10 @@ class EditTitleActivity : AppCompatActivity() {
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             checkFields()
         }
+    }
+
+    private fun isFieldsValid(): Boolean {
+        return isTitleValid() && isDescriptionValid() && isBodyValid()
     }
 
     private fun isTitleValid(): Boolean {
@@ -114,24 +208,6 @@ class EditTitleActivity : AppCompatActivity() {
     }
     //endregion
 
-    fun onClickSave(v: View) {
-        // Check fields
-        if (!isTitleValid() ||
-            !isDescriptionValid() ||
-            !isBodyValid()) { return }
-
-        if (isNewTitle) {
-            // Create a new document in "titles" collection
-            val title = titleEditText.text.toString()
-            val description = descriptionEditText.text.toString()
-            val titleBody = bodyEditText.text.toString()
-
-            // TODO - Continue..
-        } else {
-            // Edit an existing document in "titles" collection
-        }
-    }
-
     fun onClickPublish(v: View) {
         // TODO - Implement
     }
@@ -143,5 +219,9 @@ class EditTitleActivity : AppCompatActivity() {
     fun onClickBack(v: View) {
         finish()
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left)
+    }
+
+    companion object {
+        const val TAG = "EditTitleActivity"
     }
 }
