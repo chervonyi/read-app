6a6d7fb7b6d0af96c6264cfbc19679407415a1a7
chervonyi <yuri.ternopil@gmail.com>
2020-09-01 17:37:25 -0500

    Fix tabs' bug
    Replaced tabMode "auto" on "scrollable". Now, width of each tab is dynamic.

--- ReadApplication/app/src/main/java/room106/app/read/views/TitleView.kt
+++ ReadApplication/app/src/main/java/room106/app/read/views/TitleView.kt
@@ -14,7 +14,7 @@ import room106.app.read.models.LikedTitle
 import room106.app.read.models.Title
 
 @SuppressLint("ViewConstructor")
-class TitleView : LinearLayout {
+class TitleView(context: Context?, title: Title, titleID: String) : LinearLayout(context) {
 
     // Views
     private lateinit var headerTextView: TextView
@@ -22,43 +22,24 @@ class TitleView : LinearLayout {
     private lateinit var authorAvatarImageView: RoundedImageView
     private lateinit var descriptionTextView: TextView
 
-    constructor(context: Context?, title: Title, titleID: String): super(context) {
-        initView(context, title.title, title.description, title.authorName, title.authorAvatar)
-    }
+    init {
+        if (context != null && resources != null) {
+            View.inflate(context, R.layout.title_layout, this)
 
-    constructor(context: Context?, title: LikedTitle): super(context) {
-        initView(context, title.title, title.description, title.authorName, title.authorAvatar)
-    }
+            // Connect views
+            headerTextView = findViewById(R.id.titleHeaderTextView)
+            authorTextView = findViewById(R.id.titleAuthorTextView)
+            authorAvatarImageView = findViewById(R.id.titleAuthorAvatarImageView)
+            descriptionTextView = findViewById(R.id.titleDescriptionTextView)
 
-    constructor(context: Context?,
-                title: String,
-                description: String,
-                authorName: String,
-                authorAvatar: Int) : super(context) {
-        initView(context, title, description, authorName, authorAvatar)
-    }
+            // Assign data
+            headerTextView.text = title.title
+            authorTextView.text = title.authorName
+            descriptionTextView.text = title.description
 
-    private fun initView(context: Context?,
-                        title: String,
-                         description: String,
-                         authorName: String,
-                         authorAvatar: Int) {
-        View.inflate(context, R.layout.title_layout, this)
-
-        // Connect views
-        headerTextView = findViewById(R.id.titleHeaderTextView)
-        authorTextView = findViewById(R.id.titleAuthorTextView)
-        authorAvatarImageView = findViewById(R.id.titleAuthorAvatarImageView)
-        descriptionTextView = findViewById(R.id.titleDescriptionTextView)
-
-        // Assign data
-        headerTextView.text = title
-        authorTextView.text = authorName
-        descriptionTextView.text = description
-
-        val avatarName = "ic_avatar_${authorAvatar}"
-        val image = resources.getIdentifier(avatarName, "drawable", context?.packageName)
-        authorAvatarImageView.setImageResource(image)
-
+            val avatarName = "ic_avatar_${title.authorAvatar}"
+            val image = resources.getIdentifier(avatarName, "drawable", context.packageName)
+            authorAvatarImageView.setImageResource(image)
+        }
     }
 }
